f864c24e68fa756122363d0dc9531c9324a64c72
Bergson Brito Barros <bergson.barros@serpro.gov.br>
2024-10-10 11:31:25 -0300

    Remoção de import plt

--- .ipynb_checkpoints/acidentes-rodovias-brasil-streamlit-checkpoint.py
+++ .ipynb_checkpoints/acidentes-rodovias-brasil-streamlit-checkpoint.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import altair as alt
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 from vega_datasets import data
 
